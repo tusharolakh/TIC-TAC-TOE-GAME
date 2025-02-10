62a89b33a946c1c3333c04f8e8d7de9140ee4e57
Tushar Olakh <139036845+tusharolakh@users.noreply.github.com>
2025-02-10 16:50:27 +0530

    app.js

--- app.js
+++ app.js
@@ -68,42 +68,6 @@ newgamebtn.addEventListener("click" ,resetgame);
 resetbtn.addEventListener("click" ,resetgame);
 
 
-HT+ML FILE 
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TIC - TAC - TOE GAME</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="msgcontainer hide">
-        <p id="msg">WINNER</p>
-        <button id="new-btn">NEW GAME</button>
-    </div>
-  <main>
-    <h1>TIC TAC TOE</h1>
-    <div class="container">
-        <div class="game">
-          <button class="box"></button>
-          <button class="box"></button>
-          <button class="box"></button>
-          <button class="box"></button>    
-          <button class="box"></button>
-          <button class="box"></button>
-          <button class="box"></button>
-          <button class="box"></button>
-          <button class="box"></button>
-        </div>
-    </div>
-    <button id="resetbtn">RESET GAME</button>
- </main>
-    <script src = "app.js"></script>
-</body>
-</html>
 
 
 
